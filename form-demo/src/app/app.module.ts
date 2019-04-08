6058618ab6e2094be10cd05e73b91f5db2234122
EC2 Default User <ec2-user@ip-172-31-94-153.ec2.internal>
2019-04-08 07:01:19 +0000

    Form12.1

---
+++ form-demo/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
+import {   //Importare le funzionalità nella nostra applicazione
+ FormsModule,
+ ReactiveFormsModule
+} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormDemoComponent
+  ],
+  imports: [
+    BrowserModule,
+     FormsModule,  //Anche qui
+   ReactiveFormsModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
